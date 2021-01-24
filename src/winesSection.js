d8e333ba33d80add4bc93b7024e55ba8e7369fc8
Fefe <mynameisfefe@protonmail.com>
2021-01-23 23:49:34 -0300

    added title to wine page

--- src/winesSection.js
+++ src/winesSection.js
@@ -7,11 +7,18 @@ const winesDisplay = () => {
     const wine1 = document.createElement ("img");
     wine1.src = "img/wine1.jpg";
     wine1.classList.add("wine");
+    const titleandparagraph = document.createElement('div');
+    titleandparagraph.classList.add ("titleandparagraph");
     const wine1paragraph = document.createElement ("p");
     wine1paragraph.innerHTML = "A wine that blablablab <br> blablablabla <br> blablablabla <br> barbarbarbarbar"
     wine1paragraph.classList.add("paragraph");
+    const winetitle = document.createElement ("p");
+    winetitle.classList.add ("winetitle");
+    winetitle.textContent = "Malbec -2019"
+    titleandparagraph.appendChild (winetitle);
+    titleandparagraph.appendChild (wine1paragraph);
     wineandtexttogether.appendChild(wine1);
-    wineandtexttogether.appendChild(wine1paragraph);
+    wineandtexttogether.appendChild(titleandparagraph);
     winesPosition.appendChild(wineandtexttogether);
     content.appendChild (winesPosition);
 }
